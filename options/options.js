e1245c82dc83bb92e4eaef9230e50552c8b3434b
Crowdbotics <team@crowdbotics.com>
2021-08-06 22:16:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "jtrmerch_29583Identifier",
+  url: "https://jtrmerch_29583Identifier.botics.co",
+  api: "https://jtrmerch_29583Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
